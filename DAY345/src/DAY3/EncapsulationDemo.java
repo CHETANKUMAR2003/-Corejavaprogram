856cef7a3c186d07a9fafff43baecfaee3d8a78b
CHETANKUMAR2003 <chetankumarchatla@gmail.com>
2024-06-27 22:15:08 +0530

    SECOND COMMIT

---
+++ DAY345/src/DAY3/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package DAY3;
+
+public class EncapsulationDemo {
+public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(6622);
+		obj.setName("CHETAN");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+
+}
